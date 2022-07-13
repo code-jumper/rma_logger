7b29909714b23e67aba468146bca4191e2177369
Stian <stian.iverson@gmail.com>
2022-07-13 03:08:58 -0500

    Update RMA now checks all stored values against gui forms and updates as necessary

--- main.js
+++ main.js
@@ -23,21 +23,64 @@ addRMAButton.addEventListener('click', function(){
     customerLabel.innerText = 'Customer: ';
     addedRMA.append(customerLabel);
     let customer = document.createElement('input');
+    customer.name = 'customer';
     customer.value = newRMAForm[0].value;
     addedRMA.append(customer);
     let productLabel = document.createElement('label');
     productLabel.innerText = 'Product: ';
     addedRMA.append(productLabel);
     let product = document.createElement('input');
+    product.name = 'product';
     product.value = newRMAForm[1].value;
     addedRMA.append(product);
     let issueLabel = document.createElement('label');
     issueLabel.innerText = 'Issue: ';
     addedRMA.append(issueLabel);
     let issue = document.createElement('input');
+    issue.name = 'issue';
     issue.value = newRMAForm[2].value;
     addedRMA.append(issue);
     currentRMAsContainer.append(addedRMA);
     console.log(array);
 });
-
+let i = 0;
+let updateRMAsButton = document.getElementById('updateRMAs');
+updateRMAsButton.addEventListener('click', function(){
+    /// On click, check if there are any differences between stored
+    /// array of RMAs and the current displayed values in the forms.
+    /// If so, update the array with the new values
+    /// To do: Add RMA stages input (either radio or checklist)
+    for (i = 0; i<array.length; i++) {
+        if (array[i].customer != document.forms[i+1].customer.value) {
+            console.log('no match');
+            console.log(array[i].customer + ', ' + document.forms[i+1].customer.value);
+            array[i].customer = document.forms[i+1].customer.value;
+            console.log(array[i].customer + ', ' + document.forms[i+1].customer.value);
+        } else {
+            console.log('match');
+            console.log(array[i].customer + ', ' + document.forms[i+1].customer.value);
+        }
+    }
+    for (i = 0; i<array.length; i++) {
+        if (array[i].product != document.forms[i+1].product.value) {
+            console.log('no match');
+            console.log(array[i].product + ', ' + document.forms[i+1].product.value);
+            array[i].product = document.forms[i+1].product.value;
+            console.log(array[i].product + ', ' + document.forms[i+1].product.value);
+        } else {
+            console.log('match');
+            console.log(array[i].product + ', ' + document.forms[i+1].product.value);
+        }
+    }
+    for (i = 0; i<array.length; i++) {
+        if (array[i].issue != document.forms[i+1].issue.value) {
+            console.log('no match');
+            console.log(array[i].issue + ', ' + document.forms[i+1].issue.value);
+            array[i].issue = document.forms[i+1].issue.value;
+            console.log(array[i].issue + ', ' + document.forms[i+1].issue.value);
+        } else {
+            console.log('match');
+            console.log(array[i].issue + ', ' + document.forms[i+1].issue.value);
+        }
+    }
+});
